8951462250ad0534866c750e1baa26d24729745c
Anna <45884795+Usova-Anna@users.noreply.github.com>
2019-05-24 14:44:55 +0300

    Задача про яблоки на примитивные типы данных

---
+++ DimaIAnia.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class DimaIAnia {
+
+	public static void main(String[] args) {
+		float applesDima = 3.5f;
+		float applesAnia = 2.5f;
+		float sum = applesDima + applesAnia;
+		System.out.println("У Димы и Ани вместе яблок: " + sum);
+	}
+
+}
